b94a5a96f49cb0c082b931d81d23e09f0c4fb981
Samuel Kendall <sjk2g17@soton.ac.uk>
2020-03-15 06:03:42 +0000

    Changed page, seems to fix issues

--- project_workspace/RIOT/makefiles/default-radio-settings.inc.mk
+++ project_workspace/RIOT/makefiles/default-radio-settings.inc.mk
@@ -2,9 +2,9 @@ ifeq (868,$(ISM))
   ifneq (,$(filter at86rf212b,$(USEMODULE)))    # radio is IEEE 802.15.4 sub-GHz
     UK_CHANNEL ?= 0 # i.e. use 868MHz
     CFLAGS += -DIEEE802154_DEFAULT_SUBGHZ_CHANNEL=$(UK_CHANNEL)
-    UK_PAGE ?= 0 # i.e. use page 0. This corresponds to BPSK. Shouldn't actually matter here: 2 is fine
+    UK_PAGE ?= 2 # i.e. use page 2. Page 0 corresponds to BPSK. Page 2 is O-QPSK I think. Shouldn't actually matter here but it does: use 2
     CFLAGS += -DIEEE802154_DEFAULT_SUBGHZ_PAGE=$(UK_PAGE)
-    SCLK := SPI_CLK_1MHZ
+    SCLK := SPI_CLK_5MHZ
     CFLAGS += -DAT86RF2XX_PARAM_SPI_CLK=$(SCLK)
   endif
 endif
