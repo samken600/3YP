887ee6c10ff6a70e1fbb97cc6a4d7af90e84e647
Samuel Kendall <sjk2g17@soton.ac.uk>
2020-07-07 13:31:27 +0100

    Cleaning up and moving stuff

--- project_workspace/3yp_node_testing/main.c
+++ project_workspace/3yp_node_testing/main.c
@@ -19,40 +19,33 @@
 
 #include <stdio.h>
 
-//#include "periph/rtt.h"
-//#include "periph/pm.h"
+#include "periph/rtt.h"
+#include "periph/pm.h"
 //#include "net/nanocoap_sock.h"
 #include "xtimer.h"
 #include "board.h"
 #include "periph/gpio.h"
 #include "shell.h"
+#include "net/gnrc/netif.h"
 
 #define MODULE_SI7006
 
-//#define DS18_POWER_PIN GPIO_PIN(PA, 28)
-//#define DS18_PARAM_PIN GPIO_PIN(PA, 13)
 #include "si70xx_params.h"
 #include "si70xx.h"
 
-//#define COAP_INBUF_SIZE (256U)
+#define COAP_INBUF_SIZE (256U)
 
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
 /* import "ifconfig" shell command, used for printing addresses */
-/* extern int _gnrc_netif_config(int argc, char **argv);
-
-void cb(void *arg) {
-    (void)arg;
-    puts("Hello there");
-} */
-
-/* global object for ds18 sensor */
+/* extern int _gnrc_netif_config(int argc, char **argv); */
+extern gnrc_netif_t *lwmac_netif;
 
 si70xx_t dev;
 int get_temperature(int argc, char **argv);
 int get_humidity(int argc, char **argv);
-
+void _gnrc_lwmac_set_netdev_state(gnrc_netif_t *netif, netopt_state_t devstate);
 
 int toggle_led(int argc, char **argv) {
     (void)argc;
@@ -62,36 +55,67 @@ int toggle_led(int argc, char **argv) {
     return 0;
 }
 
-/*
+
 void cb(void *arg) {
     (void)arg;
-    puts("Hello there");
+    puts("RTT Callback");
 }
 
+void radio_off(gnrc_netif_t *netif) {
+    netopt_state_t devstate = NETOPT_STATE_SLEEP;
+    netif->dev->driver->set(netif->dev,
+                            NETOPT_STATE,
+                            &devstate,
+                            sizeof(devstate));
+}
+
+void radio_on(gnrc_netif_t *netif) {
+    netopt_state_t devstate = NETOPT_STATE_IDLE;
+    netif->dev->driver->set(netif->dev,
+                            NETOPT_STATE,
+                            &devstate,
+                            sizeof(devstate));
+}
+
+
 int off(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
+    int power_mode = 0;
+    if(argc == 2) {
+        int power_mode = atoi(argv[1]);
+        printf("Power Mode is: %d\n", power_mode);
+    }
+
+    gnrc_netif_t *netif = gnrc_netif_get_by_pid(7);
+    if(netif == NULL) {
+        puts("Is at86rf2xx on pid 7?");
+        return 1;
+    }
+    
+    radio_off(netif);
+    puts("Radio is sleeping...");
 
     uint32_t start = rtt_get_counter();
     rtt_set_alarm((start + 10 * RTT_FREQUENCY) & RTT_MAX_VALUE, cb, 0);
+    puts("Alarm set, sleeping");
 
-    pm_set(0);
+    pm_set(power_mode);
     
-    puts("Hey");
+    puts("Woken...");
     
     //xtimer_init();
     xtimer_sleep(1);
 
-    puts("Yo");
+    puts("Timer works");
+    radio_on(netif);
     //ps();
     return 0;
-}*/
+}
 
 static const shell_command_t shell_commands[] = {
     { "get_temp", "gets temperature in celcius", get_temperature },
     { "get_humid", "gets relative humidity", get_humidity },
     { "toggle_led", "toggles LED", toggle_led},
-//    { "off", "waits 1 second", off},
+    { "off", "waits 10 second", off},
     { NULL, NULL, NULL }
 };
 
@@ -99,7 +123,6 @@ static const shell_command_t shell_commands[] = {
 int main(void)
 {
     puts("RIOT nanocoap example application with shell and ds18 temperature sensor");
-//    rtt_init();
     /* initialise the temperature sensor */
 //    int16_t result;
 //    result = ds18_init(&dev, &(ds18_params[0]));
@@ -107,13 +130,13 @@ int main(void)
 //        puts("[Error] The sensor could not be initialised");
 //        return 1;
 //    }
-    gpio_init(GPIO_PIN(PA, 28), GPIO_OUT);
-    gpio_set(GPIO_PIN(PA,28));
+//    gpio_init(GPIO_PIN(PA, 28), GPIO_OUT);
+//    gpio_set(GPIO_PIN(PA,28));
 
-    if(si70xx_init(&dev, &si70xx_params[0]) == 0) {
-        puts("[OK]");
-    }
-    else puts("[ERROR]");
+//    if(si70xx_init(&dev, &si70xx_params[0]) == 0) {
+//        puts("[OK]");
+//    }
+//    else puts("[ERROR]");
 
     /* nanocoap_server uses gnrc sock which uses gnrc which needs a msg queue */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
@@ -130,6 +153,8 @@ int main(void)
 //                  nanocoap_thread, NULL, "nanocoap_thread");
 
     gpio_init(GPIO_PIN(PA,14), GPIO_OUT);
+
+
     /* initialize shell on board */
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
