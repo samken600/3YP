ed508cf6584c0123d7be0eeda770b7942968a44b
Samuel Kendall <sjk2g17@soton.ac.uk>
2020-07-23 12:25:01 +0100

    Added CoAP SAUL example

---
+++ project_workspace/3yp_node_coap_saul/coap_handler.c
@@ -0,0 +1,190 @@
+/*
+ * Copyright (C) 2016 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "board.h"
+#include "phydat.h"
+#include "saul_reg.h"
+
+#include "fmt.h"
+#include "net/nanocoap.h"
+
+//extern si70xx_t dev;
+
+/* internal value that can be read/written via CoAP */
+static uint8_t internal_value = 0;
+
+static const uint8_t block2_intro[] = "This is RIOT (Version: ";
+static const uint8_t block2_board[] = " running on a ";
+static const uint8_t block2_mcu[] = " board with a ";
+
+static ssize_t _riot_board_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
+{
+    (void)context;
+    return coap_reply_simple(pkt, COAP_CODE_205, buf, len,
+            COAP_FORMAT_TEXT, (uint8_t*)RIOT_BOARD, strlen(RIOT_BOARD));
+}
+
+static ssize_t _riot_block2_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
+{
+    (void)context;
+    coap_block_slicer_t slicer;
+    coap_block2_init(pkt, &slicer);
+    uint8_t *payload = buf + coap_get_total_hdr_len(pkt);
+
+    uint8_t *bufpos = payload;
+
+    bufpos += coap_put_option_ct(bufpos, 0, COAP_FORMAT_TEXT);
+    bufpos += coap_opt_put_block2(bufpos, COAP_OPT_CONTENT_FORMAT, &slicer, 1);
+    *bufpos++ = 0xff;
+
+    /* Add actual content */
+    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, block2_intro, sizeof(block2_intro)-1);
+    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, (uint8_t*)RIOT_VERSION, strlen(RIOT_VERSION));
+    bufpos += coap_blockwise_put_char(&slicer, bufpos, ')');
+    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, block2_board, sizeof(block2_board)-1);
+    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, (uint8_t*)RIOT_BOARD, strlen(RIOT_BOARD));
+    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, block2_mcu, sizeof(block2_mcu)-1);
+    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, (uint8_t*)RIOT_MCU, strlen(RIOT_MCU));
+    /* To demonstrate individual chars */
+    bufpos += coap_blockwise_put_char(&slicer, bufpos, ' ');
+    bufpos += coap_blockwise_put_char(&slicer, bufpos, 'M');
+    bufpos += coap_blockwise_put_char(&slicer, bufpos, 'C');
+    bufpos += coap_blockwise_put_char(&slicer, bufpos, 'U');
+    bufpos += coap_blockwise_put_char(&slicer, bufpos, '.');
+
+
+    unsigned payload_len = bufpos - payload;
+    return coap_block2_build_reply(pkt, COAP_CODE_205,
+                                   buf, len, payload_len, &slicer);
+}
+
+static ssize_t _riot_value_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
+{
+    (void) context;
+
+    ssize_t p = 0;
+    char rsp[16];
+    unsigned code = COAP_CODE_EMPTY;
+
+    /* read coap method type in packet */
+    unsigned method_flag = coap_method2flag(coap_get_code_detail(pkt));
+
+    switch(method_flag) {
+    case COAP_GET:
+        /* write the response buffer with the internal value */
+        p += fmt_u32_dec(rsp, internal_value);
+        code = COAP_CODE_205;
+        break;
+    case COAP_PUT:
+    case COAP_POST:
+    {
+        /* convert the payload to an integer and update the internal value */
+        char payload[16] = { 0 };
+        memcpy(payload, (char*)pkt->payload, pkt->payload_len);
+        internal_value = strtol(payload, NULL, 10);
+        code = COAP_CODE_CHANGED;
+    }
+    }
+
+    return coap_reply_simple(pkt, code, buf, len,
+            COAP_FORMAT_TEXT, (uint8_t*)rsp, p);
+}
+
+static ssize_t _temp_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context) {
+    (void) context;
+
+    ssize_t p = 0;
+    char rsp[64];
+    unsigned code = COAP_CODE_EMPTY;
+    saul_reg_t *dev;
+
+    // get temp in celsius
+    SENSOR_POWER_ON;
+
+    phydat_t res;
+    dev = saul_reg_find_type(SAUL_SENSE_TEMP);
+    if(dev == NULL) {
+        puts("error: no temperature sensors found");
+        return coap_reply_simple(pkt, COAP_CODE_NOT_IMPLEMENTED, buf, len, 
+                                 COAP_FORMAT_TEXT, (uint8_t*)rsp, p);
+    }
+
+    int dim = saul_reg_read(dev, &res);
+    if(dim <= 0) {
+        puts("error: failed to read from device");
+        return coap_reply_simple(pkt, COAP_CODE_INTERNAL_SERVER_ERROR, buf, len,
+                                 COAP_FORMAT_TEXT, (uint8_t*)rsp, p);
+    }
+
+    SENSOR_POWER_OFF;
+    
+    p = phydat_to_json(&res, dim, rsp);
+    code = COAP_CODE_CONTENT;
+    
+    return coap_reply_simple(pkt, code, buf, len,
+            COAP_FORMAT_JSON, (uint8_t*)rsp, p);
+}
+
+static ssize_t _led_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context) {
+    (void)context;
+
+    int dim;
+    ssize_t p = 0;
+    char rsp[16];
+    unsigned code = COAP_CODE_EMPTY;
+    saul_reg_t *dev;
+    phydat_t data;
+
+    dev = saul_reg_find_type(SAUL_ACT_SWITCH);
+    if(dev == NULL) {
+        puts("error: no led/switch found");
+        return coap_reply_simple(pkt, COAP_CODE_NOT_IMPLEMENTED, buf, len, 
+                                 COAP_FORMAT_TEXT, (uint8_t*)rsp, p);
+    }
+
+    memset(&data, 0, sizeof(data));
+    char payload[16] = { 0 };
+    memcpy(payload, (char*)pkt->payload, pkt->payload_len);
+
+    char *end;
+    data.val[0] = (strtol(payload, &end, 10) > 0) ? 1 : 0;
+    if(end==payload || *end != '\0' || errno == ERANGE) {
+        puts("error: entered string is not number");
+        return coap_reply_simple(pkt, COAP_CODE_UNSUPPORTED_CONTENT_FORMAT, buf, len,
+                                 COAP_FORMAT_TEXT, (uint8_t*)rsp, p);
+
+    }
+    dim = saul_reg_write(dev, &data);
+
+    if(dim <= 0) {
+        puts("error: failed to write to device");
+        return coap_reply_simple(pkt, COAP_CODE_INTERNAL_SERVER_ERROR, buf, len,
+                                 COAP_FORMAT_TEXT, (uint8_t*)rsp, p);
+    }
+
+    code = COAP_CODE_CHANGED;
+
+    return coap_reply_simple(pkt, code, buf, len,
+            COAP_FORMAT_TEXT, (uint8_t*)rsp, p);
+}
+
+/* must be sorted by path (ASCII order) */
+const coap_resource_t coap_resources[] = {
+    COAP_WELL_KNOWN_CORE_DEFAULT_HANDLER,
+    { "/led", COAP_POST | COAP_PUT, _led_handler, NULL },
+    { "/riot/board", COAP_GET, _riot_board_handler, NULL },
+    { "/riot/value", COAP_GET | COAP_PUT | COAP_POST, _riot_value_handler, NULL },
+    { "/riot/ver", COAP_GET, _riot_block2_handler, NULL },
+    { "/temp", COAP_GET, _temp_handler, NULL },
+};
+
+const unsigned coap_resources_numof = ARRAY_SIZE(coap_resources);
---
+++ project_workspace/3yp_node_coap_saul/main.c
@@ -0,0 +1,145 @@
+/*
+ * Copyright (C) 2016 Kaspar Schleiser <kaspar@schleiser.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       CoAP example server application (using nanocoap)
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @}
+ */
+
+#include <stdio.h>
+
+//#include "periph/rtt.h"
+//#include "periph/pm.h"
+#include "net/nanocoap_sock.h"
+#include "xtimer.h"
+#include "board.h"
+#include "periph/gpio.h"
+#include "shell.h"
+
+/*
+#define MODULE_SI7006
+#include "si70xx_params.h"
+#include "si70xx.h"
+*/
+
+#define COAP_INBUF_SIZE (256U)
+
+#define MAIN_QUEUE_SIZE     (8)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+/* import "ifconfig" shell command, used for printing addresses */
+extern int _gnrc_netif_config(int argc, char **argv);
+
+/**/
+char nanocoap_thread_stack[THREAD_STACKSIZE_MAIN];
+void *nanocoap_thread(void *arg) {
+    (void) arg;
+
+    /* initialise nanocoap server */
+    uint8_t buf[COAP_INBUF_SIZE];
+    sock_udp_ep_t local = { .port=COAP_PORT, .family=AF_INET6 };
+    nanocoap_server(&local, buf, sizeof(buf));
+
+    /* should never return */
+    return NULL;
+}
+
+/* global object for si70xx sensor */
+//si70xx_t dev;
+//int get_temperature(int argc, char **argv);
+//int get_humidity(int argc, char **argv);
+
+int toggle_led(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    LED0_TOGGLE;
+    return 0;
+}
+
+int toggle_sensor(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
+    SENSOR_POWER_TOGGLE;
+    return 0;
+}
+
+static const shell_command_t shell_commands[] = {
+//    { "get_temp", "gets temperature in celcius", get_temperature },
+//    { "get_humid", "gets relative humidity", get_humidity },
+    { "toggle_led", "toggles LED", toggle_led },
+    { "toggle_sensor", "toggles sensor power", toggle_sensor },
+    { NULL, NULL, NULL }
+};
+
+
+int main(void)
+{
+    puts("RIOT nanocoap example application with shell and si70xx temperature sensor");
+    /* turn on the temp sensor and init power toggle pin */
+
+    /* initialise the temperature sensor */
+    //if(si70xx_init(&dev, &si70xx_params[0]) == 0) {
+    //    puts("[OK]");
+    //}
+    //else puts("[ERROR]");
+
+    /* nanocoap_server uses gnrc sock which uses gnrc which needs a msg queue */
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+
+    puts("Waiting for address autoconfiguration...");
+    xtimer_sleep(3);
+
+    /* initialize nanocoap server instance by starting thread */
+    thread_create(nanocoap_thread_stack, sizeof(nanocoap_thread_stack),
+                  THREAD_PRIORITY_MAIN + 1, THREAD_CREATE_STACKTEST,
+                  nanocoap_thread, NULL, "nanocoap_thread");
+
+    /* initialize shell on board */
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+
+    /* should be never reached */
+    return 0;
+}
+
+/*
+int get_temperature(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+    int16_t temperature;
+    puts("Getting temp...\n");
+
+    / get temp in celsius /
+    temperature = si70xx_get_temperature(&dev);
+    printf("temperature: %d.%02d C\n", temperature / 100, temperature % 100);
+
+    return 0;
+}
+
+int get_humidity(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+    uint16_t humid;
+    puts("Getting humidity...\n");
+
+    / get temp in celsius /
+    humid = si70xx_get_relative_humidity(&dev);
+    printf("relative humidity: %d.%02d\n", humid / 100, humid % 100);
+
+    return 0;
+}*/
+
