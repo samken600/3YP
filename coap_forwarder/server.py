7187c36fb896bf459ffd86727109c67b517f78e9
Samuel Kendall <sjk2g17@soton.ac.uk>
2020-07-27 12:40:59 +0100

    Adding folders to run CoAP server and forwarder. Server will run in parallel on VPS and store in db, forwarder will do ipv6 -> ipv4 on laptop

---
+++ coap_forwarder/server.py
@@ -0,0 +1,39 @@
+from coapthon.server.coap import CoAP
+from coapthon.resources.resource import Resource
+from coapthon.defines import Codes, Content_types
+
+class BasicResource(Resource):
+    def __init__(self, name="BasicResource", coap_server=None):
+        super(BasicResource, self).__init__(name)
+        self.payload = "Basic Resource"
+    
+    def render_GET_advanced(self, request, response):
+        response.payload = self.payload
+        response.code = Codes.CONTENT.number
+        response.content_type = Content_types["text/plain"]
+        return self, response
+    
+    def render_PUT_advanced(self, request, response):
+        self.payload = request.payload
+        from coapthon.messages.response import Response
+        assert(isinstance(response, Response))
+        response.payload = "PUT success"
+        response.code = Codes.CHANGED.number
+        return self, response
+
+class CoAPServer(CoAP):
+    def __init__(self, host, port):
+        CoAP.__init__(self, (host, port))
+        self.add_resource('/temp', BasicResource())
+
+def main():
+    server = CoAPServer("::", 5683)
+    try:
+        server.listen(10)
+    except KeyboardInterrupt:
+        print("Server shutdown")
+        server.close()
+        print("Exiting...")
+
+if __name__ == "__main__":
+    main()
