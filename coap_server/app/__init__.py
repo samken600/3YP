16ae21ec0f1a3649d6bb747d3ee43aa4c64974b7
Samuel Kendall <sjk2g17@soton.ac.uk>
2020-07-27 12:21:33 +0000

    setting up files

---
+++ coap_server/app/__init__.py
@@ -0,0 +1,8 @@
+''' coapthon app with mongo '''
+
+import os, json, datetime
+from bson.objectid import ObjectId
+from flaks import Flask
+from flask_pymongo import PyMongo
+
+
---
+++ coap_server/run.py
@@ -0,0 +1,7 @@
+# run server
+import sys, os
+
+from app import app
+
+if __name__ == '__main__':
+    # run the app boi
