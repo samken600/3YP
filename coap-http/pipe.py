3abbb4cc2b4570f07746db70ccc992c973ec1960
Samuel Kendall <sjk2g17@soton.ac.uk>
2020-07-29 23:06:53 +0100

    Updated to fit new API

--- coap-http/pipe.py
+++ coap-http/pipe.py
@@ -12,23 +12,26 @@ clients = []
 
 for host in hosts:
     client = HelperClient(server=(host['ip'], port))
-    clients.append({'client': client, 'number': host['number']})
-
+    hw_array = host['ip'].split(":")[-4:]
+    hw_array[0] = "%x" % (int(hw_array[0], 16) + 512)
+    hwaddr = "".join(hw_array)
+    hwaddr = ":".join(hwaddr[i:i+2] for i in range(0, len(hwaddr), 2)).upper()
+    clients.append({'client': client, 'number': host['number'], 'hwaddr': hwaddr})
 
 responses = []
 for client in clients:
-    now = datetime.now()
+    now = datetime.utcnow()
     response = client['client'].get(path=path, timeout=15) # pretty sure timeout is in seconds
     if response is not None:
         print(response.pretty_print())
-        responses.append({'time': now, 'response': response, 'number': client['number']})
+        responses.append({'time': now, 'response': response, 'number': client['number'], 'hwaddr': client['hwaddr']})
     client['client'].stop()
 
 for response in responses:
     if response['response'].code is 69: # 69 = content
         data = {
-                    'time': response['time'].isoformat(),
-                    'node': response['number'],
+                    'time': response['time'].timestamp(),
+                    'hwaddr': response['hwaddr'],
                     'temp': int(response['response'].payload)/100
                }
         print(data)
