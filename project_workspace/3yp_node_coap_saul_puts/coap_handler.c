39f3324b002ee6dbd45f83f079b9f3812274478f
Samuel Kendall <sjk2g17@soton.ac.uk>
2020-07-30 22:46:02 +0100

    Removed unneeded functions

--- project_workspace/3yp_node_coap_saul_puts/coap_handler.c
+++ project_workspace/3yp_node_coap_saul_puts/coap_handler.c
@@ -19,15 +19,6 @@
 #include "fmt.h"
 #include "net/nanocoap.h"
 
-//extern si70xx_t dev;
-
-/* internal value that can be read/written via CoAP */
-static uint8_t internal_value = 0;
-
-static const uint8_t block2_intro[] = "This is RIOT (Version: ";
-static const uint8_t block2_board[] = " running on a ";
-static const uint8_t block2_mcu[] = " board with a ";
-
 static ssize_t _riot_board_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
 {
     (void)context;
@@ -35,71 +26,6 @@ static ssize_t _riot_board_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, vo
             COAP_FORMAT_TEXT, (uint8_t*)RIOT_BOARD, strlen(RIOT_BOARD));
 }
 
-static ssize_t _riot_block2_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
-{
-    (void)context;
-    coap_block_slicer_t slicer;
-    coap_block2_init(pkt, &slicer);
-    uint8_t *payload = buf + coap_get_total_hdr_len(pkt);
-
-    uint8_t *bufpos = payload;
-
-    bufpos += coap_put_option_ct(bufpos, 0, COAP_FORMAT_TEXT);
-    bufpos += coap_opt_put_block2(bufpos, COAP_OPT_CONTENT_FORMAT, &slicer, 1);
-    *bufpos++ = 0xff;
-
-    /* Add actual content */
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, block2_intro, sizeof(block2_intro)-1);
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, (uint8_t*)RIOT_VERSION, strlen(RIOT_VERSION));
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, ')');
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, block2_board, sizeof(block2_board)-1);
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, (uint8_t*)RIOT_BOARD, strlen(RIOT_BOARD));
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, block2_mcu, sizeof(block2_mcu)-1);
-    bufpos += coap_blockwise_put_bytes(&slicer, bufpos, (uint8_t*)RIOT_MCU, strlen(RIOT_MCU));
-    /* To demonstrate individual chars */
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, ' ');
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, 'M');
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, 'C');
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, 'U');
-    bufpos += coap_blockwise_put_char(&slicer, bufpos, '.');
-
-
-    unsigned payload_len = bufpos - payload;
-    return coap_block2_build_reply(pkt, COAP_CODE_205,
-                                   buf, len, payload_len, &slicer);
-}
-
-static ssize_t _riot_value_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context)
-{
-    (void) context;
-
-    ssize_t p = 0;
-    char rsp[16];
-    unsigned code = COAP_CODE_EMPTY;
-
-    /* read coap method type in packet */
-    unsigned method_flag = coap_method2flag(coap_get_code_detail(pkt));
-
-    switch(method_flag) {
-    case COAP_GET:
-        /* write the response buffer with the internal value */
-        p += fmt_u32_dec(rsp, internal_value);
-        code = COAP_CODE_205;
-        break;
-    case COAP_PUT:
-    case COAP_POST:
-    {
-        /* convert the payload to an integer and update the internal value */
-        char payload[16] = { 0 };
-        memcpy(payload, (char*)pkt->payload, pkt->payload_len);
-        internal_value = strtol(payload, NULL, 10);
-        code = COAP_CODE_CHANGED;
-    }
-    }
-
-    return coap_reply_simple(pkt, code, buf, len,
-            COAP_FORMAT_TEXT, (uint8_t*)rsp, p);
-}
 
 static ssize_t _temp_handler(coap_pkt_t *pkt, uint8_t *buf, size_t len, void *context) {
     (void) context;
@@ -225,8 +151,6 @@ const coap_resource_t coap_resources[] = {
     COAP_WELL_KNOWN_CORE_DEFAULT_HANDLER,
     { "/led", COAP_POST | COAP_PUT, _led_handler, NULL },
     { "/riot/board", COAP_GET, _riot_board_handler, NULL },
-    { "/riot/value", COAP_GET | COAP_PUT | COAP_POST, _riot_value_handler, NULL },
-    { "/riot/ver", COAP_GET, _riot_block2_handler, NULL },
     { "/temp", COAP_GET, _temp_handler, NULL },
     { "/txpower", COAP_PUT, _txpow_handler, NULL },
 };
