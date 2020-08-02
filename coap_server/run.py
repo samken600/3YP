4e7e503d42e24188a42569cd632f13757160b7bf
Samuel Kendall <sjk2g17@soton.ac.uk>
2020-08-02 00:30:10 +0000

    Moving coap_server to local coapthon3

--- coap_server/run.py
+++ coap_server/run.py
@@ -3,6 +3,9 @@ import sys, os
 #from config import dbinfo
 import app
 
+import logging
+
 if __name__ == '__main__':
     # run the app boi
+    logging.basicConfig(level=logging.INFO)
     app.run()
