f82433ca8b630ab714de68d5f74d81a2adf80d76
Samuel Kendall <sjk2g17@soton.ac.uk>
2020-08-05 16:20:00 +0100

    Removed unneeded code

--- coap_forwarder/server.py
+++ coap_forwarder/server.py
@@ -1,31 +1,9 @@
 import logging
 from coapthon.forward_proxy.coap import CoAP
-from coapthon.resources.resource import Resource
-from coapthon.defines import Codes, Content_types
-
-class BasicResource(Resource):
-    def __init__(self, name="BasicResource", coap_server=None):
-        super(BasicResource, self).__init__(name)
-        self.payload = "Basic Resource"
-    
-    def render_GET_advanced(self, request, response):
-        response.payload = self.payload
-        response.code = Codes.CONTENT.number
-        response.content_type = Content_types["text/plain"]
-        return self, response
-    
-    def render_PUT_advanced(self, request, response):
-        self.payload = request.payload
-        from coapthon.messages.response import Response
-        assert(isinstance(response, Response))
-        response.payload = "PUT success"
-        response.code = Codes.CHANGED.number
-        return self, response
 
 class CoAPServer(CoAP):
     def __init__(self, host, port):
         CoAP.__init__(self, (host, port))
-#        self.add_resource('/temp', BasicResource())
 
 def main():
     logging.basicConfig(level=logging.INFO)
